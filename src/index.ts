51fd1144aa7adc55f6f080cbbf2c311ed8141911
Jahidul Islam <jahid.redmi10@gmail.com>
2025-10-07 20:37:44 +0600

    update

--- src/index.ts
+++ src/index.ts
@@ -4,33 +4,40 @@ const prisma = new PrismaClient();
 
 async function main() {
   // create data in a table
-  const result = await prisma.user.create({
-    data: {
-      name: "jahankar mahbub",
-      email: "jahid.redmi10@gmail.com",
-      profilePhoto: "https://programming_hero.png",
-    },
-  });
-  console.log(result);
+  // const result = await prisma.user.create({
+  //   data: {
+  //     name: "jahankar mahbub",
+  //     email: "jahid.redmi10@gmail.com",
+  //     profilePhoto: "https://programming_hero.png",
+  //   },
+  // });
+  // console.log(result);
   // find add data fromtable
-  const userdata = await prisma.user.findMany();
-
+  // const userdata = await prisma.user.findMany();
   //find data using id
-  const userdata = await prisma.user.findMany({
+  // const userdata = await prisma.user.findMany({
+  //   where: {
+  //     id: 1,
+  //   },
+  // });
+  // get single data in object
+  // const findUser = await prisma.user.findUnique({
+  //   where: {
+  //     id: 2,
+  //   },
+  // });
+  // console.log(findUser);
+
+  const update = await prisma.user.update({
     where: {
       id: 1,
     },
-  });
-  
-
-  // get single data in object
-
-  const findUser = await prisma.user.findUnique({
-    where: {
-      id: 2,
+    data: {
+      name: "mejba",
+      email: "mejaba@gmail.com",
     },
   });
-  console.log(findUser);
+  console.log(update);
 }
 
 main();
