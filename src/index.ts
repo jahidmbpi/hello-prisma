358dbbd872728d9b51a81c0e097b9b61b47537b4
Jahidul Islam <jahid.redmi10@gmail.com>
2025-10-07 20:31:26 +0600

    update

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,27 @@ import { PrismaClient } from "../generated/prisma/index.js";
 const prisma = new PrismaClient();
 
 async function main() {
-  // const result = await prisma.user.create({
-  //   data: {
-  //     name: "jahankar mahbub",
-  //     email: "jahid.redmi10@gmail.com",
-  //     profilePhoto: "https://programming_hero.png",
-  //   },
-  // });
-  // console.log(result);
+  // create data in a table
+  const result = await prisma.user.create({
+    data: {
+      name: "jahankar mahbub",
+      email: "jahid.redmi10@gmail.com",
+      profilePhoto: "https://programming_hero.png",
+    },
+  });
+  console.log(result);
+  // find add data fromtable
+  const userdata = await prisma.user.findMany();
 
-  // const userdata = await prisma.user.findMany({
-  //   where: {
-  //     id: 1,
-  //   },
-  // });
+  //find data using id
+  const userdata = await prisma.user.findMany({
+    where: {
+      id: 1,
+    },
+  });
+  
+
+  // get single data in object
 
   const findUser = await prisma.user.findUnique({
     where: {
